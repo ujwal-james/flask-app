6ae98f79ffafb9dc91aeedace50901b7eace1ed1
ujwal-james <127179951+ujwal-james@users.noreply.github.com>
2023-05-25 22:48:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 import pandas as pd
 import sqlite3
 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,17 +18,13 @@ def create():
     cursor = connection.cursor()
 
     
-
-    # Create the table
     cursor.execute("CREATE TABLE IF NOT EXISTS production (well_number TEXT, oil INT, gas INT, brine INT)")
 
-    # Insert the data into the table
     for _, row in annual_data.iterrows():
-        # print(row["OIL"])
+       
         cursor.execute("INSERT INTO production VALUES (?, ?, ?, ?)",
                     (row.name, row["OIL"], row["GAS"], row["BRINE"]))
 
-    # connection close
     connection.commit()
     connection.close()
     return "db created   <br> <a href=http://localhost:8080/data?well=34059242540000>test sample for http://localhost:8080/data?well=34059242540000</a>  <br> <br><form action=./data?well=data> <input type=text name=well placeholder=data required /> <br><br> <input type=submit value=submit></form>"
@@ -38,12 +33,10 @@ def create():
 @app.route("/data", methods=["GET"])
 def get_data():
     well_number = request.args.get("well")
-    # well_number='34059242540000'
 
     connection = sqlite3.connect("production.sqlite")
     cursor = connection.cursor()
 
-    # Retrieve the data for the requested well
     cursor.execute("SELECT oil, gas, brine FROM production WHERE well_number = ?", (well_number,))
     result = cursor.fetchone()
     connection.close()
